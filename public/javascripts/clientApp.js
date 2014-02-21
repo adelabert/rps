673d2b47a6395cec111d49ffb1b63dc5eb1f13dd
Zach Markin <zachmarkin@gmail.com>
2014-02-20 19:11:00 -0500

    checking the waiting queue for a match

--- public/javascripts/clientApp.js
+++ public/javascripts/clientApp.js
@@ -5,54 +5,13 @@ socket.on('frmServer:login', function(){
 	socket.emit('frmClient:login', username);
 });
 
-socket.on('frmServer:refreshRoom', function(room){
-	renderRoom(room);
+socket.on('startingHigher', function(players){
+	alert('starting a game with a lower player');
+});
+socket.on('startingLower', function(players){
+	alert('starting a game with a higher player');
 });
 
 
-function renderRoom(room){
-	renderUsers(room.users);
-	//renderTables(room);
-}
 
 
-function renderUsers(users){
-	var usersHTML = '';
-	function scriptLine(element, index, array){
-		var temp = '';
-		temp += '' + index + ': ' + element.username + '</br>'	;
-		usersHTML += temp;
-	}
-	users.forEach(scriptLine);
-	$('#userList').html(usersHTML);
-}
-
-/*
-function renderTables(room){
-	var tablesHTML = '';
-	for(var i=0; i<room.tables.length; i++){
-		var tempHTML = scriptTable(i, room.users, room.waitList);
-		tablesHTML += tempHTML;
-	}		
-	$('#room').html(tablesHTML);
-}
-
-
-function scriptTable(high, low, waiting){
-	var highChairHTML = scriptChair('high', high);
-	var lowChairHTML = scriptChair('low', low);
-	var waitChairHTML = srciptChair('wait', wait);
-	var tableHTML = highChairHTML + lowChairHTML + waitChairHTML;
-	return tableHTML;
-}
-
- 
-function scriptChair(chairType, playerName){
-	var temp = '<div class="chair ' + chairType
-						 + '" data-player="' + playerName
-						 + '" data-type="' + chairType
-						 + '">';
-	 temp += playerName;
-	 temp += '</div>';
-	return temp;
-}*/
--- routes/sockets.js
+++ routes/sockets.js
@@ -12,32 +12,82 @@ module.exports = function(sockets) {
 
 	//room info
 	var room = {
-		'users': []
+		'users': [],
+		'games': [],
+		'rankings':[],
+		'waiting': []
 	};
 
 	//user constructor
-	function User(socketId, username){
-		this.socketId = socketId;
+	function User(socket, username, rank){
+		this.socketId = socket.id;
 		this.username = username;
-		this.free 		= true;
+		this.socket   = socket;
+		this.rank     = rank;
 	}
 
 	//deleting a user from the room
-	function pruneUser(socketId, users){
+	function pruneUser(socketId, users, waiting){
 		for(var i=0; i<users.length; i++){
 			if(users[i].socketId==socketId){
 				users.splice(i,1);
 				break;
 			}
 		}
+		for(var i=0; i<waiting.length; i++){
+			if(waiting[i].socketId==socketId){
+				waiting.splice(i,1);
+				break;
+			}
+		}
+		checkWaiting(waiting);
 	}
 
 	//adding a user to the room
-	function addUser(socketId, username, userArray){
-		var tempUser = new User(socketId, username);
-		userArray.push(tempUser);
+	function addUser(socket, username, users, waiting){
+		var tempUser = new User(socket, username, users.length);
+		users.push(tempUser);
+		waiting.push(tempUser);
+		checkWaiting(waiting);
 	}
 
+	function checkWaiting(waiting){
+		for(var i=0; i<(waiting.length-1); i++){
+			var opponent = checkForMatch(i, waiting);
+			if(opponent){
+				startGame(waiting[i], opponent);
+				var opponentIndex = (waiting.indexOf(opponent)-1);
+				waiting.splice(i,1);
+				waiting.splice(opponentIndex,1);
+				checkWaiting(waiting);
+			}
+		}
+	}
+
+	function checkForMatch(i, waiting){
+		var current = waiting[i];
+		i++;
+		var baseReturn = null;
+		for(a=i; a<waiting.length; a++){
+			var temp = waiting[a];
+			if((Math.abs(temp.rank-current.rank))<=2){
+				return temp;
+			}
+		}
+		return baseReturn;
+	}
+
+	function startGame(higher, lower){
+		var clientHigher = {'username': higher.username,
+												'rank': higher.rank
+												}
+		var clientLower = {'username': lower.username,
+											 'rank': lower.rank 
+											}
+		var players = {'higher':clientHigher, 'lower':clientLower};
+		higher.socket.emit('startingHigher', players);
+		lower.socket.emit('startingLower', players);
+	}
 
 	function toAll(socket, toHappen, data){
 		socket.emit(toHappen, data);
@@ -48,13 +98,11 @@ module.exports = function(sockets) {
 	sockets.on('connection', function(socket){
 
 		socket.on('frmClient:login', function(username){
-			addUser(socket.id, username, room.users);
-			toAll(socket, 'frmServer:refreshRoom', room);
+			addUser(socket, username, room.users, room.waiting);
 		});
 		
 		socket.on('disconnect', function(){
-			pruneUser(socket.id, room.users);
-			socket.broadcast.emit('frmServer:refreshRoom', room);
+			pruneUser(socket.id, room.users, room.waiting);
 		});
 
 		socket.emit('frmServer:login');
